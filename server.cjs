c6f22095ff2fa4b95a75bd6c648ac252f43e4263
selvashri <selvashri.ee21@bitsathy.ac.in>
2024-02-13 15:31:02 +0530

    cors

--- server.cjs
+++ server.cjs
@@ -1,8 +1,10 @@
 const express = require('express')
+const cors = require('cors')
 const bodyParser=require('body-parser')
 const {ObjectId} = require('mongodb')
 const {connectToDb, getDb} = require('./dbconnection.cjs')
 const app = express()
+app.use(cors())
 app.use(bodyParser.json())
 
 let db
